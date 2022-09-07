aea5188820c50ec461e5792d24be16f90a664e21
Gitea <gitea@fake.local>
2022-09-07 22:04:14 +0800

    已登录状态下不能进入登录界面

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -10,11 +10,20 @@ const whiteList = ["/login", "/register", "/404"];
 router.beforeEach((to, from, next) => {
   // user -> token
   if (whiteList.includes(to.path) || localStorage.getItem("USER_INFO")) {
+    console.log("to: ", to.path);
+    console.log("from ", from.path);
+    if (
+      (to.path == "/login" || to.path == "/register") &&
+      localStorage.getItem("USER_INFO")
+    ) {
+      console.log("已登录, 请求被驳回, ", from.path);
+      next("/" || from.path);
+    }
     document.title = to.meta.title;
     next();
+    // }
   } else {
     next("/login");
-    // next("/");
   }
 });
 
